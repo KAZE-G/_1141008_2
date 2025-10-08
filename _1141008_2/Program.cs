d2cae1759ea7d1b5de639eb5bfc76c0413d46b72
KAZE_laptop <t112590028@ntut.org.tw>
2025-10-08 15:47:14 +0800

    加入專案檔案。

---
+++ _1141008_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1141008_2
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// 應用程式的主要進入點。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            // MessageBox.Show("你想要顯示的內容！");
+            
+        }
+    }
+}
